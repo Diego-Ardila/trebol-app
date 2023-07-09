f756635c554813a8712fa9125ecc1ef92c7b4ea1
Diego Ardila <dardila90@gmail.com>
2023-07-09 13:06:48 -0500

    Fixing layout responsiness in Documents view

--- src/pages/Documents.tsx
+++ src/pages/Documents.tsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect } from 'react';
 import './Documents.scss';
 import FileUploadInputs from '../components/Documents/FileUploadInputs';
+import StepButtons from '../components/global/StepButtons';
 
 function Documents() {
 
   return (
     <div className="documents">
       <FileUploadInputs />
+      <StepButtons />
     </div>
   );
 }
